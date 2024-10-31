1257eef598277298edf042300d8cdd74441d4bbe
archengdev <106987393+archengdev@users.noreply.github.com>
2024-10-31 22:34:21 +0000

    saving work

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -12,4 +12,28 @@ class MiscController < ApplicationController
     @film_list = Movie.all.where({:director_id => @id })
     render({ :template => "misc_templates/dir_id"})
   end
+
+  def movies
+    @movie_list = Movie.all
+    render({ :template => "misc_templates/movies"})
+  end
+
+  def movie_id
+    @id = params.fetch("id")
+    @movie = Movie.all.where({:id => @id})[0]
+    @dir = Director.all.where({:id => @movie.director_id})[0]
+    render({ :template => "misc_templates/movie_id"})
+  end
+
+  def actors
+    @actor_list = Actor.all
+    render({ :template => "misc_template/actors"})
+  end
+
+  def actor_id
+    @id = params.fetch("id")
+    @actor = Actor.all.where({:id => @id})[0]
+    @films = Character.all.where({:actor_id => @id})
+    render({ :template => "misc_template/actor_id"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/directors/:id", {:controller => "misc", :action => "dir_id"})
   get("/movies", {:controller => "misc", :action => "movies"})
   get("/movies/:id", {:controller => "misc", :action => "movie_id"})
+  get("/actors", {:controller => "misc", :action => "actors"})
+  get("/actors/:id", {:controller => "misc", :action => "actor_id"})
 end
