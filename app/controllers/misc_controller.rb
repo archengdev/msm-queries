d1f850cce77177e820d7409edbf9af15032edb51
archengdev <106987393+archengdev@users.noreply.github.com>
2024-10-31 21:53:10 +0000

    saving work

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,14 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  def directors
+    @dir_list = Director.all
+    render({ :template => "misc_templates/directors"})
+  end
+  def dir_id
+    @id = params.fetch("id")
+    @dir = Director.all.where({:id => @id})[0]
+    @film_list = Movie.all.where({:director_id => @id })
+    render({ :template => "misc_templates/dir_id"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "directors"})
+  get("/directors/:id", {:controller => "misc", :action => "dir_id"})
+  get("/movies", {:controller => "misc", :action => "movies"})
+  get("/movies/:id", {:controller => "misc", :action => "movie_id"})
 end
